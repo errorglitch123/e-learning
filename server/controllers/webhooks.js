35b405f96b8926e01b370e988b17f89a081eb2f1
Aditya Meena <adityameena1602@gmail.com>
2026-08-05 13:16:46 +0530

    modified:   server/controllers/webhooks.js

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -66,6 +66,26 @@ export const stripeWebhooks = async (req,res)=>{
   }
   // handle the event
    switch (event.type) {
+    case 'payment_intent.succeeded': {
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId,
+      })
+      const {purchaseId} = session.data[0].metadata;
+      const purchaseData = await Purchase.findById(purchaseId);
+      const userData = await User.findById(purchaseData.userId);
+      const courseData = await Course.findById(purchaseData.courseId.toString());
+
+      courseData.enrolledStudents.push(userData._id);
+      await courseData.save();
+
+      userData.enrolledCourses.push(courseData._id);
+      await userData.save()
+      purchaseData.status = 'completed';
+      await purchaseData.save();
+      break;
+    }
     case 'checkout.session.completed': {
       const session = event.data.object;
       const {purchaseId} = session.metadata;
